b4b0cc0d7012688fd5835ad47823d456ab6571d7
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-02 12:51:52 +1300

    raw_input to read a string from the keyboard + concatenate two strings

---
+++ ex12.py
@@ -0,0 +1,11 @@
+
+#raw_input to read a string from the keyboard
+#concatenate two strings
+distance_1=raw_input("what distance did you cross today ?")
+distance_2=raw_input("what distance did you cross yesterday ?")
+
+distance_total=distance_1+distance_2
+
+print "so you crossed %s in two days" % distance_total,
+print "so you crossed in two days", distance_1+distance_2,
+print distance_1+distance_2

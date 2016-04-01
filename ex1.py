68ae2aa8ce0b4237de05c5f73fb881e401acfa13
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-01 23:26:40 +1300

    ex1:print "If I add %d and %d I get %d." % (age, some_int, age+ some_int)

--- ex1.py
+++ ex1.py
@@ -10,3 +10,9 @@ print 7/4
 
 a_distance = 4.1
 print "the distance is", a_distance, "km."
+
+name = "hassan"
+age = 43
+some_int = 6
+print "my name %s is the best name" % name
+print "If I add %d and %d I get %d." % (age, some_int, age+ some_int)

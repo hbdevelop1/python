12546baf140f160d8bfb8bdc292c044842345139
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-02 20:06:38 +1300

    lib of functions to run in python interactive mode

---
+++ lib_cal.py
@@ -0,0 +1,20 @@
+#how to add a lib of functions to python ?
+#1-in a lib_cal.py file add the following code
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def prod(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+    
+#2-compile to make sure there are no errors 
+#3-enter in python interactive mode: $python
+#4- >>>import lib_cal
+#5- >>>print lib_cal.add(5,4)
+    

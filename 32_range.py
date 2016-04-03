6c75da3f739984bd4eba3193bdf38de47a07a5bd
hbdevelop1 <https://hbdevelop1@github.com>
2016-04-03 22:42:55 +1200

    range for numbers + exit()

---
+++ 32_range.py
@@ -0,0 +1,12 @@
+#range for numbers + exit()
+
+numbers=range(10)
+print numbers, " note that 10 is not included in the list"
+
+
+numbers=range(0,10)
+print numbers
+exit(0)
+numbers=range(0,10,3)
+print numbers
+
